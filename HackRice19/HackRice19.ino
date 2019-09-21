0d79953b1ddb811d8113385c8f75e7fd71e3c1ba
[Thomas Russo] <russo517@yahoo.com>
2019-09-21 00:07:34 -0500

    Update HackRice19.ino

--- HackRice19/HackRice19.ino
+++ HackRice19/HackRice19.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+// What pins the sensors are connected to
+#define FLEXSENSORPIN0 A0 
+#define FLEXSENSORPIN1 A1 
+#define GYROSENSORPIN A2
 
+//defined variables for each sensor
+float middleReading;
+float indexReading;
+float gyroReading;
+
+float clickerPosition
+ 
+void setup(void) {
+  Serial.begin(9600);
 }
+ 
+void loop(void) {
+  middleReading = analogRead(FLEXSENSORPIN0);
+  indexReading = analogRead(FLEXSENSORPIN1);
+  gyroReading = analogRead(GYROSENSORPIN);
 
-void loop() {
-  // put your main code here, to run repeatedly:
+  clickerPosition = map(gyroReading, 0, 1023, 0, 90);
 
+  Serial.print("Reading Middle "); 
+  Serial.println(middleReading);
+
+  Serial.print("Reading Index "); 
+  Serial.println(indexReading);
+
+  Serial.print("Reading Gyro "); 
+  Serial.println(gyroReading);
+
+  Serial.print("Clicker Position "); 
+  Serial.println(clickerPosition);
 }
