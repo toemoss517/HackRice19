ddf52c0edc8df26a9e6549fb2261db3e3ffb854c
Michael Thomas Russo <russo517@yahoo.com>
2019-09-22 16:54:39 -0500

    Update HackRice19.ino

--- HackRice19/HackRice19.ino
+++ HackRice19/HackRice19.ino
@@ -1,66 +1,60 @@
+#include <Adafruit_Sensor.h>
+#include <Adafruit_L3GD20_U.h>
+
 #include <Mouse.h>
-#define FLEXSENSORPIN0 A0 
-#define FLEXSENSORPIN1 A1 
-#define GYROSENSORPIN A2
+
+
+#define FLEXSENSORPIN0 A0
+#define FLEXSENSORPIN1 A1
+
+Adafruit_L3GD20_Unified gyro = Adafruit_L3GD20_Unified(20);
+bool begin(gyroRange_t L3DS20_RANGE_500DPS);
 
 
 //defined variables for each sensor
 float middleReading;
 float indexReading;
-float gyroReading;
-float clickerPosition;
 
 void setup(void) {
   Serial.begin(9600);
-  Mouse.begin();
+  //Mouse.begin();
+
 
 }
- 
-void loop(void) {
-  middleReading = analogRead(FLEXSENSORPIN0);
-  indexReading = analogRead(FLEXSENSORPIN1);
-  gyroReading = analogRead(GYROSENSORPIN);
-  clickerPosition = map(gyroReading, 0, 1023, 0, 90);
 
-  //Mouse reading
-  int XReading = clickerPosition;
-  int YReading = clickerPosition*(-1);
+void loop(void) {
+  middleReading = analogRead(FLEXSENSORPIN1);
+  indexReading = analogRead(FLEXSENSORPIN0);
+
+  sensors_event_t event; 
+  Serial.print(2);
+  gyro.getEvent(&event);
+  Serial.print(3);
   
-  //Move the Mouse
-  Mouse.move(XReading, YReading, 0);
-  delay(10);
-  
+  Serial.print("X: "); Serial.print(event.gyro.x); Serial.print("  ");
+  Serial.print("Y: "); Serial.print(event.gyro.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.gyro.z); Serial.print("  ");
+
   //Testing the Sensors
-      Serial.print("Reading Middle "); 
-      Serial.println(middleReading);
-      
-      delay(300);
-      
-      //Serial.print("Reading Index "); 
-      //Serial.println(indexReading);
-    
-      delay(300);
-      
-      //Serial.print("Reading Gyro "); 
-      //Serial.println(gyroReading);
-    
-      delay(300);
-      
-      //Serial.print("Clicker Position "); 
-      //Serial.println(clickerPosition);
+//  Serial.print("Reading Middle ");
+//  Serial.println(middleReading);
+//
+//  Serial.print("Reading Index ");
+//  Serial.println(indexReading);
+
+  delay(300);
+
   //End of Testing
 
-  if (middleReading < 775) {
-    Mouse.click(MOUSE_LEFT);
-  }
-  
-  if (indexReading < 775) {
-    Mouse.click(MOUSE_RIGHT);
-  }
+//  if (middleReading < 400) {
+//    Mouse.click(MOUSE_LEFT);
+//  }
+//
+//  if (indexReading < 475) {
+//    Mouse.click(MOUSE_RIGHT);
+//  }
 
   delay(100);
 
 
 }
-
- 
